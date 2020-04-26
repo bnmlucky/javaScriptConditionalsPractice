d55227e70f9a25d6aee708795c5de48ac7a41873
Natalia Titova <bnmlucky@gmail.com>
2020-04-26 11:58:32 -0400

    created a larger else if statement for different outcomes

---
+++ js/else-if.js
@@ -0,0 +1,12 @@
+const weather = '';
+
+
+if (weather === 'sun') {
+    console.log("it's sunny, so I'm going swimming.");
+} else if (weather === 'rain') {
+    console.log("It's raining, so I will read a book.");
+} else if (weather === 'snow') {
+    console.log("It's snowing, so I'm going sledding.")
+} else {
+    console.log("I don't know what I'm doing today.");
+}
